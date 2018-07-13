f1520241a02eb23cb052709e33bede86fe39e692
chenbo <1531516103@qq.com>
2018-07-13 14:28:12 +0800

    vue硅谷外卖项目

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import {Button} from 'mint-ui'
+import './mock/mockServer'
+
+Vue.component(Button.name,Button)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router,
+  store
+})
---
+++ src/store/mutations.js
@@ -0,0 +1,43 @@
+import {
+  RECEIVE_ADDRESS,
+  RECEIVE_CATEGORYS,
+  RECEIVE_SHOPS,
+  RECEIVE_USER,
+  RESET_USER,
+  RECEIVE_INFO,
+  RECEIVE_RATINGS,
+  RECEIVE_GOODS
+} from './mutation-types'
+
+export default {
+  [RECEIVE_ADDRESS] (state, {address}) {
+    state.address = address
+  },
+
+  [RECEIVE_CATEGORYS] (state, {categorys}) {
+    state.categorys = categorys
+  },
+
+  [RECEIVE_SHOPS] (state, {shops}) {
+    state.shops = shops
+  },
+
+  [RECEIVE_USER] (state, {user}) {
+    state.user = user
+  },
+
+  [RESET_USER] (state) {
+    state.user = {}
+  },
+
+  [RECEIVE_GOODS] (state, {goods}) {
+    state.goods = goods
+  },
+  [RECEIVE_RATINGS] (state, {ratings}) {
+    state.ratings = ratings
+  },
+  [RECEIVE_INFO] (state, {info}) {
+    state.info = info
+  },
+
+}
